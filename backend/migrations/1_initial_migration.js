c39d9c852cb3f0cf1087bad0a459a7c40bdc0ec8
Luis Seyboldt <lui.seyboldt@gmail.com>
2020-12-12 12:23:19 +0100

    fixed migration error

--- backend/migrations/1_initial_migration.js
+++ backend/migrations/1_initial_migration.js
@@ -4,5 +4,6 @@ const DHBWVoting = artifacts.require("DHBWVoting");
 const bvs_backend = artifacts.require("bvs_backend")
 
 module.exports = function (deployer) {
+  deployer.deploy(Migrations);
   deployer.deploy(bvs_backend);
 };
